66bb51ce805e0c6e94821d7da0269cb33b717100
Viorel Budeci <viorel.budeci@amdaris.com>
2020-02-04 17:23:13 +0200

    Add JPlayer for play music

--- src/app/library/songs/song-detail/song-detail.component.ts
+++ src/app/library/songs/song-detail/song-detail.component.ts
@@ -2,11 +2,15 @@ import { Component, OnInit, Output, Input } from '@angular/core';
 import { Song } from 'src/app/shared/song.model';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { SongService } from '../song.service';
+declare var jQuery: any;
 
 @Component({
   selector: 'app-song-detail',
   templateUrl: './song-detail.component.html',
-  styleUrls: ['./song-detail.component.css']
+  styleUrls: [
+    './song-detail.component.css', 
+    './jplayer.vimusicapp.css'
+  ]
 })
 export class SongDetailComponent implements OnInit {
   song: Song;
@@ -17,6 +21,52 @@ export class SongDetailComponent implements OnInit {
               ) { }
 
   ngOnInit() {
+
+    (function ($) {
+      $(document).ready(function(){
+        console.log("Hello from jQuery!");
+        console.log(jQuery("p"));
+
+        // jQuery.jPlayerPlaylist = new  jPlayerPlaylist({
+        //   jPlayer: "#jquery_jplayer_1",
+        //   cssSelectorAncestor: "#jp_container_1"
+        // }, 
+        // [
+        //   {
+        //     mp3:"../../../../data/audios/0bc6e5c4-1488-4454-b4f8-17b1cc1c951c.mp3"
+        //   }
+        //   ,
+        //   {
+        //     mp3:"https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+        //   }
+        //   ,
+        //   {
+        //     mp3:"http://www.jplayer.org/audio/mp3/TSP-07-Cybersonnet.mp3"
+        //   }
+        // ], 
+        // {
+        //   swfPath: "../../../../../node_modules/jplayer/dist/jplayer",
+        //   supplied: "oga, mp3",
+        //   wmode: "window",
+        //   useStateClassSkin: true,
+        //   autoBlur: false,
+        //   smoothPlayBar: true,
+        //   keyEnabled: true
+        // });
+
+
+        $("#jpId").jPlayer( {
+          ready: function () {
+            $(this).jPlayer("setMedia", {
+              mp3: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"// Defines the mp3 url
+            });
+          }
+        });
+
+
+      });
+    })(jQuery);
+
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.song = this.songService.getSong(this.id);
